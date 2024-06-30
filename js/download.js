cbc2a2ad0768a64427ec354276777d06210f4306
ronaldreagan <gamitinmoangemail@gmail.com>
2024-06-30 16:18:40 +0800

    new

--- js/download.js
+++ js/download.js
@@ -66,68 +66,68 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Function to detect device type
     function detectDevice() {
-        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-        if (/android/i.test(userAgent)) {
-            return 'android';
-        }
-        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            return 'ios';
-        }
-        return 'unknown';
-    }
+		const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+		if (/android/i.test(userAgent)) {
+			return 'android';
+		}
+		if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+			return 'ios';
+		}
+		return 'unknown';
+	}
 
     // Function to copy text to clipboard
     function copyText(text, callback) {
-        var tag = document.createElement("input");
-        tag.setAttribute("id", "cp_hgz_input");
-        tag.value = text;
-        document.getElementsByTagName("body")[0].appendChild(tag);
-        document.getElementById("cp_hgz_input").select();
-        document.execCommand("copy");
-        document.getElementById("cp_hgz_input").remove();
-        if (callback) {
-            callback(text);
-        }
-    }
+		var tag = document.createElement("input");
+		tag.setAttribute("id", "cp_hgz_input");
+		tag.value = text;
+		document.getElementsByTagName("body")[0].appendChild(tag);
+		document.getElementById("cp_hgz_input").select();
+		document.execCommand("copy");
+		document.getElementById("cp_hgz_input").remove();
+		if (callback) {
+			callback(text);
+		}
+	}
 
     // Function to get query parameters from URL
     function getQueryVariable(variable) {
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        for (var i = 0; i < vars.length; i++) {
-            var pair = vars[i].split("=");
-            if (pair[0] === variable) {
-                return pair[1];
-            }
-        }
-        return "";
-    }
+		var query = window.location.search.substring(1);
+		var vars = query.split("&");
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split("=");
+			if (pair[0] === variable) {
+				return pair[1];
+			}
+		}
+		return "";
+	}
 
     // Function to handle software download based on device type
     async function DownSoft() {
-        var u = navigator.userAgent;
-        var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
-        var isAndroid = u.indexOf("Android") > -1 || u.indexOf("Adr") > -1;
-
-        let gameId = getQueryVariable("from_gameid");
-        let code = getQueryVariable("channelCode");
-        let copyObj = {
-            from_gameid: gameId,
-            channelCode: code,
-        };
-        let copyStr = JSON.stringify(copyObj);
-        copyText(copyStr, function () {
-            console.log("Copy successful", copyStr);
-        });
-
-        if (isiOS) {
-            window.location.href = "https://zbgmg.hjfoycmy.com/api/c/xttclkk8"; // Replace with your iOS download URL
-        } else if (isAndroid) {
-            window.location.href = "https://appsvip.s3.ap-south-1.amazonaws.com/rummmytry.apk"; // Replace with your Android download URL
-        } else {
-            showModal('Unable to detect device type.');
-        }
-    }
+		var u = navigator.userAgent;
+		var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+		var isAndroid = u.indexOf("Android") > -1 || u.indexOf("Adr") > -1;
+	
+		let gameId = getQueryVariable("from_gameid");
+		let code = getQueryVariable("channelCode");
+		let copyObj = {
+			from_gameid: gameId,
+			channelCode: code,
+		};
+		let copyStr = JSON.stringify(copyObj);
+		copyText(copyStr, function () {
+			console.log("Copy successful", copyStr);
+		});
+	
+		if (isiOS) {
+			window.location.href = "https://zbgmg.hjfoycmy.com/api/c/xttclkk8"; // Replace with your iOS download URL
+		} else if (isAndroid) {
+			window.location.href = "https://appsvip.s3.ap-south-1.amazonaws.com/rummmytry.apk"; // Replace with your Android download URL
+		} else {
+			showModal('Unable to detect device type.');
+		}
+	}
 
     // Modal handling (assuming you have a modal with id 'myModal')
     const modal = document.getElementById('myModal');
@@ -135,9 +135,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const span = document.getElementsByClassName('close')[0];
 
     function showModal(message) {
-        modalMessage.textContent = message;
-        modal.style.display = "block";
-    }
+		const modal = document.getElementById('myModal');
+		const modalMessage = document.getElementById('modalMessage');
+		modalMessage.textContent = message;
+		modal.style.display = "block";
+	}
 
     if (span) {
         span.onclick = function() {
